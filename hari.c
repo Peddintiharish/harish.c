a53263b7a9b3f844c0eb59903a20751c89fc909c
Peddintiharish <93182569+Peddintiharish@users.noreply.github.com>
2021-11-15 12:42:51 +0530

    Add files via upload

---
+++ hari.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int a, b, c;
+	printf("enter 1st number");
+	scanf("%d",&a);
+    printf("enter the 2nd number");
+    scanf("%d" ,&b);
+    printf("enter the 3rd number");
+    scanf("%d",&c);
+   
+}
---
+++ hari1436.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+	int a,b, c;
+	printf("enter the three numbers");
+		scanf("%d%d%d",&a,&b,&c);
+	printf("%c%c%c",a+92,b+96,c+104);
+}
